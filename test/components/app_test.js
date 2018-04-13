c9a441ab46e520f983ff225a185c0cd1b8034a41
Andrew Locke <alocke12992@gmail.com>
2018-04-13 14:22:51 -0600

    Basic structure of a test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,13 @@
-import { renderComponent , expect } from '../test_helper';
+import {renderComponent, expect} from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+//Use 'describe' to group together similar tests
+describe('App')
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+// Use 'it' to test a single attribute of a target
+// The string inside here is only used for output in the console if it passes
+it('shows the correct text')
+
+// Use 'expect' to make an 'assertion' about target
+expect()
