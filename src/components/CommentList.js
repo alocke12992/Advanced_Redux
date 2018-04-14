2200e087fabfada45a4d51c52a8083e526e01850
Andrew Locke <alocke12992@gmail.com>
2018-04-14 13:11:43 -0600

    Adds redux store to comment list

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 const CommentList = (props) => {
+  const list = props.comments.map(comment =>
+    <li key={comment}>{comment}</li>
+  )
+
   return (
-    <div className='comment-list'>
-      This is the list
-    </div>
+    <ui className='comment-list'>
+      {list}
+    </ui>
   )
 }
 
-export default CommentList
+const mapStateToProps = (state) => {
+  return {comments: state.comments}
+}
+export default connect(mapStateToProps)(CommentList)
