c2609d29e10a6c3f73409862ff537a4683b9b21e
Andrew Locke <alocke12992@gmail.com>
2018-04-14 11:50:11 -0600

    Refactors app_test to remove initial tests

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,17 +3,13 @@ import App from '../../src/components/app';
 
 //Use 'describe' to group together similar tests
 describe('App', () => {
-  // Use 'it' to test a single attribute of a target
-  // The string inside here is only used for output in the console if it passes
-  it('shows the correct text', () => {
-    // Create an instance of App
-    const component = renderComponent(App)
-    // Use 'expect' to make an 'assertion' about target
-    expect(component).to.contain('React simple starter');
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App)
   })
 
   it('renders a comment box', () => {
-    const component = renderComponent(App)
     expect(component.find('.comment-box')).to.exist
   })
 })
