7011425b26e81430a3a558fd5121d7ecbb173c77
Andrew Locke <alocke12992@gmail.com>
2018-04-14 10:15:07 -0600

    First test is passing

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,11 +3,17 @@ import App from '../../src/components/app';
 
 
 //Use 'describe' to group together similar tests
-describe('App')
+describe('App', () => {
+  // Use 'it' to test a single attribute of a target
+  // The string inside here is only used for output in the console if it passes
+  it('shows the correct text', () => {
+    // Create an instance of App
+    const component = renderComponent(App)
 
-// Use 'it' to test a single attribute of a target
-// The string inside here is only used for output in the console if it passes
-it('shows the correct text')
+    // Use 'expect' to make an 'assertion' about target
+    expect(component).to.contain('React simple starter');
 
-// Use 'expect' to make an 'assertion' about target
-expect()
+  })
+
+})
+
