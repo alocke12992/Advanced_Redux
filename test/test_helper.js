67f4a0421fa52f1071328ce0cb4f29dbd77d4a8d
Andrew Locke <alocke12992@gmail.com>
2018-04-19 19:35:24 -0600

    What we need to do

---
+++ test/test_helper.js
@@ -0,0 +1,8 @@
+//Set up testing environtment  to run like a browser in the command line 
+
+// build 'renderComponent' helper that should render a given react class
+
+// Build helper for simulating events
+
+// Set up chai-jquery
+
